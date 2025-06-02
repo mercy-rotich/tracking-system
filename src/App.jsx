81956364e2c8e5df2545b1a3e6dbee02f035c04f
mercy-rotich <cherotichm182@gmail.com>
2025-06-02 12:58:17 +0300

    changes on the folder structure and imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import AdminSidebar from './components/Admin/AdminSidebar/AdminSidebar'
 import AdminHeader from './components/Admin/AdminHeader/AdminHeader'
+import AdminLayout from './components/Admin/AdminLayout'
 
 const App = () => {
   
   return (
     <div>
-    <AdminHeader/>
-    <AdminSidebar/>
+    <AdminLayout/>
       
     </div>
   )
---
+++ src/components/Admin/AdminLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AdminHeader from './AdminHeader/AdminHeader'
+import AdminSidebar from './AdminSidebar/AdminSidebar'
+
+const AdminLayout = () => {
+  return (
+    <div>
+      <AdminHeader/>
+      <AdminSidebar/>
+    </div>
+  )
+}
+
+export default AdminLayout
