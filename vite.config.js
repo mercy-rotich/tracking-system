0746b5c85d476ca10dd43b19fa9b6ae29716a5f8
mercy-rotich <cherotichm182@gmail.com>
2025-06-19 19:47:11 +0300

    all updates

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://kz017zc2-8090.inc1.devtunnels.ms',
+        changeOrigin: true,
+        secure: true,
+      }
+    }
+  }
 })
