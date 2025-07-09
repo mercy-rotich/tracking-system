e882015fcadc0e3c222b57f7b1c8fb24ca29831a
mercy-rotich <cherotichm182@gmail.com>
2025-07-09 23:44:26 +0300

    updates

--- src/components/Admin/AdminLayout.jsx
+++ src/components/Admin/AdminLayout.jsx
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import AdminHeader from './AdminHeader/AdminHeader';
 import AdminSidebar from './AdminSidebar/AdminSidebar';
+import AdminDashboardOverview from '../../Pages/Admin/AdminDashboardOverview/AdminDashboardOverview';
+import AdminCurriculaPage from '../../Pages/Admin/AdminCurriculaPage/AdminCurriculaPage';
+import SystemMonitoringPage from '../../Pages/Admin/SystemMonitoringPage/SystemMonitoringPage';
+import Reports from '../../Pages/Admin/ReportsPage/Reports';
 
 const AdminLayout = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -15,6 +19,10 @@ const AdminLayout = () => {
       <AdminHeader onToggleSidebar={toggleSidebar} />
       <AdminSidebar isOpen={sidebarOpen} onToggle={toggleSidebar} />
       <main className="admin-main-content">
+        <AdminDashboardOverview/>
+        <AdminCurriculaPage/>
+        <SystemMonitoringPage/>
+        <Reports/>
        
         <Outlet />
       </main>
